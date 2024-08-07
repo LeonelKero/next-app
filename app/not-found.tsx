3f7428e52a5a41fc96ed7ae1e31af552fa856aea
leonel ka <waboleonel@gmail.com>
2024-08-07 20:02:02 +0100

    Code refactoring, remove typo

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,5 @@
-
 const AppPageNotFound = () => {
-  return <div>That page you are looking for doesn't exist.</div>;
+  return <div>That page you are looking for does not exist.</div>;
 };
 
 export default AppPageNotFound;
