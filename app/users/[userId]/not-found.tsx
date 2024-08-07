31febe95279bd037d0dd4d38e2977ed19e2f85f5
leonel ka <waboleonel@gmail.com>
2024-08-07 19:55:08 +0100

    Show not found page at the specific module level

---
+++ app/users/[userId]/not-found.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserNotFoundPage = () => {
+  return <div>Sorry, user not found!</div>;
+};
+
+export default UserNotFoundPage;
--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,9 +1,12 @@
+import { notFound } from "next/navigation";
 
 interface Props {
   params: { userId: number };
 }
 
 const UserDetails = ({ params: { userId } }: Props) => {
+  if (userId > 10) notFound();
+
   return <div>UserDetails {userId}</div>;
 };
 
