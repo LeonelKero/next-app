46bbd61741b322eec709b11a10e186b5a6bca96a
leonel ka <waboleonel@gmail.com>
2024-08-11 12:20:28 +0100

    Define PUT method

--- app/api/books/[id]/route.tsx
+++ app/api/books/[id]/route.tsx
@@ -16,4 +16,22 @@ const GET = (request: NextRequest, { params: { id } }: Props) => {
   );
 };
 
+const PUT = async (request: NextRequest, { params: { id } }: Props) => {
+  const book = (await request.json()) as Book;
+  if (parseInt(id, 10) > 10 || !book.title || !book.releasedAt)
+    return NextResponse.json(
+      { message: "Unable to perform update" },
+      { status: 403 }
+    );
+
+  return NextResponse.json(
+    {
+      id: book.id,
+      title: book.title.concat(" UPDATED "),
+      releasedAt: book.releasedAt,
+    },
+    { status: 200 }
+  );
+};
+
 export { GET };
