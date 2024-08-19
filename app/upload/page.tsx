a6dbe737b58a6a02ed126073c8037d074884e63e
leonel ka <waboleonel@gmail.com>
2024-08-19 22:54:35 +0100

    Display uploaded image asset

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,10 +1,30 @@
 "use client";
-import { CldUploadWidget } from "next-cloudinary";
+import { CldImage, CldUploadWidget } from "next-cloudinary";
+import { useState } from "react";
+
+interface CloudinaryUploadResult {
+  public_id: string;
+}
 
 function UploadFile() {
+  const [uploadedAsset, setUploadedAsset] = useState<CloudinaryUploadResult>();
+
   return (
     <div>
-      <CldUploadWidget uploadPreset="dhlkcxzu">
+      {uploadedAsset?.public_id && (
+        <CldImage
+          src={uploadedAsset?.public_id}
+          width={300}
+          height={200}
+          alt="Uploaded image"
+        />
+      )}
+      <CldUploadWidget
+        uploadPreset="dhlkcxzu"
+        onSuccess={(result, options) => {
+          setUploadedAsset(result?.info as CloudinaryUploadResult);
+        }}
+      >
         {({ open }) => (
           <button className="btn btn-secondary" onClick={() => open()}>
             Upload
