0e999cfac57cf9783500f139b5d422d04aac26aa
leonel ka <waboleonel@gmail.com>
2024-08-21 22:10:24 +0100

    Update route

--- middleware.ts
+++ middleware.ts
@@ -10,5 +10,5 @@ export const config = {
     // * -> for zero or more characters, Then /users/:id* match "/users";  "/users/[1-100..."; "/users/3/any"; etc
     // + -> for one or more characters,  Then /users/:id+ at least one id
     // ? -> for zero or one character,  Then /users/:id? match "/users" or "/users/anyId"
-    matchers: ["/users"]
+    matchers: ["/users/:id*"] // THis the protected routes
 }
