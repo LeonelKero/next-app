3534e4c6ec2cd7824b0f95c1be8f39c6cc662333
leonel ka <waboleonel@gmail.com>
2024-08-21 22:08:32 +0100

    Define middleware for protected routes

---
+++ middleware.ts
@@ -0,0 +1,14 @@
+import middleware from 'next-auth/middleware';
+
+// export function middleware(request: NextRequest) {
+//     return NextResponse.redirect(new URL("/new-page", request.url))
+// }
+
+export default middleware;
+
+export const config = {
+    // * -> for zero or more characters, Then /users/:id* match "/users";  "/users/[1-100..."; "/users/3/any"; etc
+    // + -> for one or more characters,  Then /users/:id+ at least one id
+    // ? -> for zero or one character,  Then /users/:id? match "/users" or "/users/anyId"
+    matchers: ["/users"]
+}
