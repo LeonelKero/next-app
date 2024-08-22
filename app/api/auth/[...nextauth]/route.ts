97a50a9998e5fbbd25d83430ec5b12593d9b5659
leonel ka <waboleonel@gmail.com>
2024-08-22 21:47:56 +0100

    Make app session support jWT

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,19 @@
 import prisma from "@/prisma/client";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from 'next-auth/providers/google';
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
     adapter: PrismaAdapter(prisma),
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!
         })
-    ]
+    ],
+    session: {
+        strategy: "jwt"
+    }
 }
 
 const handler = NextAuth(authOptions)
