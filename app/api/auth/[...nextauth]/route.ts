6e536b8baa083a6f0f546da983a609a4d96868e1
leonel ka <waboleonel@gmail.com>
2024-08-24 23:34:33 +0100

    Credentials implementation - discouraged approach

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,24 @@
 import prisma from "@/prisma/client";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import NextAuth, { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from 'next-auth/providers/google';
 
 export const authOptions: NextAuthOptions = {
     adapter: PrismaAdapter(prisma),
     providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: { label: "Your email", type: "email", placeholder: "User email here" },
+                password: { label: "Your password", type: "password", placeholder: "Password" }
+            },
+            async authorize(credentials, req) {
+                if (!credentials?.password || !credentials.email) return null
+                const user = await prisma.user.findUnique({ where: { email: credentials.email } })
+                return user
+            },
+        }),
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!
