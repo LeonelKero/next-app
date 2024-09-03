7bc09440044119b35d16bb8339a5b1ce133be4b3
leonel ka <waboleonel@gmail.com>
2024-09-03 21:50:30 +0100

    Code refactoring

--- app/api/register/route.tsx
+++ app/api/register/route.tsx
@@ -17,17 +17,21 @@ export const POST = async (request: NextRequest) => {
       { error: bodyValidation.error.errors },
       { status: 400 }
     );
+
   // As schema matches
-  // Check if there no other user with the sane credentials
+  // Check if there no other user with the sane credentials aka email
   const potentialUser = await prisma.user.findUnique({
     where: { email: bodyValidation.data.email },
   });
+
   // If user does exist
   if (potentialUser)
     return NextResponse.json({ error: "User already esxist" }, { status: 400 });
+
   // Otherwise register this new user
   // First Hash the passeword
   const hashedPassword = await bcrypt.hash(bodyValidation.data.password, 10);
+  
   // Then persist user data
   const newUser = await prisma.user.create({
     data: { email: bodyValidation.data.email, hashedPassword },
