bc789d4b6a32ad8e79abfb749d7dbaf8d504daf1
leonel ka <waboleonel@gmail.com>
2024-09-04 10:15:54 +0100

    Define api route to send email

---
+++ app/api/send-email/route.tsx
@@ -0,0 +1,19 @@
+import WelcomeTemplate from "@/emails/WelcomeTemplate";
+import { NextResponse } from "next/server";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const sendEmail = async () => {
+  const { data, error } = await resend.emails.send({
+    from: "",
+    to: "mishidevxyz@gmail.com",
+    subject: "VCS Account verification",
+    react: WelcomeTemplate({ name: "leonel ka" }),
+  });
+
+  if (error)
+    return NextResponse.json({ error: error.message }, { status: 400 });
+
+  return NextResponse.json({ data }, { status: 200 });
+};
